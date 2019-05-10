36f2fa4966d365013f0cd721397da927424daa9d
Gideon <gideonpinto123@gmail.com>
2019-05-10 01:25:40 -0400

    Check errors

--- chat/src/firebase.js
+++ chat/src/firebase.js
@@ -4,7 +4,7 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyD6SnbnnsATC4mvAds5XCnUVGY7ox0a4yI',
+	apiKey: '',
 	authDomain: 'firechat-d8a74.firebaseapp.com',
 	databaseURL: 'https://firechat-d8a74.firebaseio.com',
 	projectId: 'firechat-d8a74',
